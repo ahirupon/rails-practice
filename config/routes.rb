29a45bca3204b9a9791794373ded30e26f67910c
EC2 Default User <ec2-user@ip-172-31-47-175.us-east-2.compute.internal>
2019-02-17 07:30:09 +0000

    finish 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get 'user/new'
+
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'user#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#hello'
+  root 'static_pages#home'
 end
---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  test 'layouts test' do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    get contact_path
+    assert_select 'title', full_title("Contact")
+  end
+  #   assert true
+  # end
+end
+
+# assert_select "div"	<div>foobar</div>
+# assert_select "div", "foobar"	<div>foobar</div>
+# assert_select "div.nav"	<div class="nav">foobar</div>
+# assert_select "div#profile"	<div id="profile">foobar</div>
+# assert_select "div[name=yo]"	<div name="yo">hey</div>
+# assert_select "a[href=?]", ’/’, count: 1	<a href="/">foo</a>
+# assert_select "a[href=?]", ’/’, text: "foo"	<a href="/">foo</a>
