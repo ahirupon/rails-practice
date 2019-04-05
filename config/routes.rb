8c89a27c653c65284aa33f20ba1ab2ace5252a97
EC2 Default User <ec2-user@ip-172-31-47-175.us-east-2.compute.internal>
2019-04-05 09:00:38 +0000

    update route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get 'signup', to: 'users#new'
-  post 'signup', to: 'users#create'
+  post 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
